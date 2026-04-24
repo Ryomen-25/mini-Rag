2ea75b4a63cdb090b598db5ce2dc67a29c850b07
Ryomen-25 <m.m.reda.2200@gmail.com>
2026-04-24 21:21:25 +0300

    update instructions

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from mini-rag!")
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return{
+        "message": "Mini RAG is running"
+    }
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/welcome")
+def welcome():
+    return{
+        "message": "Hello"
+    }
